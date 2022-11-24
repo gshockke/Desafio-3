d2c5848468c7c319bfc4675d9f4e58acec293460
Paulista <41450122+gshockke@users.noreply.github.com>
2022-11-24 11:45:46 -0300

    app.py, template, venv

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/quem_somos")
+def quem_somos():
+    return render_template('quem_somos.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
